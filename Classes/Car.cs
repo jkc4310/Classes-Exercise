d60173bfd2d6f773d0401accc51d58fd79169871
kendall chapman <jkc4310@gmail.com>
2023-07-31 20:51:02 -0400

    Classes exercise 1

---
+++ Classes/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car()
+		{
+			
+		}
+
+        public string Make { get; set; }
+		public string Model { get; set; }
+		public string Year { get; set; }
+
+		public Car (string make, string model, int year)
+		{
+			make = make;
+			model = model;
+			year = year;
+
+		}
+    }
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car
+            {
+                Year = "2011",
+                Make = "Subaru",
+                Model = "Forester"
+            };
+
+
+
+            var ferrari = new Car()
+            {
+                Make = "ferrari",
+                Model = "La Ferrari",
+                Year = "2015",
+            };
+
+
+            var Toyota = new Car("Toyota", "Tacoma", 2014);
+
+
+
+            var carList = new List<Car>() { myCar, ferrari, Toyota };
+
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Year} {vehicle.Make} {vehicle.Model}");
+            }
         }
+
     }
 }
